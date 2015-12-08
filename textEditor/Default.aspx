0dc249cd16cd95a0540a43d905b6fb09745f8ea8
brandond12 <brandonjunk1@hotmail.com>
2015-12-08 13:26:37 -0500

    Added project

---
+++ textEditor/Default.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="textEditor._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <script type="text/javascript" src="script/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript">
+
+        function getFiles() {
+
+            //debuging to make sure it gets in function
+            document.getElementById("txtbx_editingArea").innerHTML = "Got Here";
+
+            $.ajax({
+                type: "GET",
+                url: "Default.aspx/testPage",
+                contentType: "application/json; charset=utf-8",
+                data: "{}",
+                //dataType: "json",
+                success: function (response) {
+                    //var files = JSON.parse(response.d);
+                    document.getElementById("txtbx_editingArea").innerHTML = "Responce Succesfull";
+                },
+                failure: function (msg) {
+                    document.getElementById("txtbx_editingArea").innerHTML = "Responce fail";
+                }
+            });
+        }
+    </script>  
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:ListBox ID="lstbx_fileSelection" runat="server" Width="465px"></asp:ListBox>
+                <br />
+                <asp:Button ID="btn_Open" runat="server" OnClientClick="getFiles()" Text="Open" />
+                <br />
+                <br />
+                <asp:TextBox ID="txtbx_editingArea" runat="server" Height="281px" TextMode="MultiLine" Width="468px"></asp:TextBox>
+                <br />
+                <br />
+                <asp:Label ID="Lbl_FileName" runat="server" Text="File Name"></asp:Label>
+                <br />
+                <asp:TextBox ID="txtbx_SaveAs" runat="server" Width="250px"></asp:TextBox>
+                <asp:Button ID="btn_Save" runat="server" OnClick="btn_Save_Click" Text="Save" />
+                <br />
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
+
+
---
+++ textEditor/Default.aspx.cs
@@ -0,0 +1,107 @@
+﻿/*
+* FILE : Default.aspx.cs
+* PROJECT : PROG 2000 - Text Editor with AJAX
+* PROGRAMMER : Brandon Davies
+* FIRST VERSION : 2015-12/02
+* DESCRIPTION : This program is a basic text editor that allows the user to open a file, edit contents and save to a file
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.Script.Services;
+using System.Web.Script.Serialization;
+
+namespace textEditor
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true)]
+        public string testPage()
+        {
+            return "Yay";
+        }
+
+
+        [WebMethod]
+        //[ScriptMethod(UseHttpGet = true)]
+        public string Page_Load()
+        {
+            //generate list boxs files
+            try
+            {
+                DirectoryInfo fileInfo = new DirectoryInfo(@"\MyFiles");
+                FileInfo[] filesFromFolder = fileInfo.GetFiles("*.txt");
+
+                //send to json string
+
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+                return serializer.Serialize(filesFromFolder).ToString();
+                /*
+                //loop through all found files
+                foreach (FileInfo file in files)
+                {
+                    bool there = false;
+                    //loop through all items in list box
+                    foreach(ListItem item in lstbx_fileSelection.Items)
+                    {
+                        if(item.Text == file.Name)
+                        {
+                            there = true;//if the found file and item in list box match, this file is already added
+                        }
+                    }
+                    //if file wasnt there, add it
+                    if (!there)
+                    {
+                        lstbx_fileSelection.Items.Add(file.Name);
+                    }
+                 
+                }*/
+            
+            }
+            catch(Exception)
+            {
+                //opening directory failed
+                //show error message and disable features that wont work
+                System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE=JavaScript>alert(\"\\MyFiles not found\")</SCRIPT>");
+                btn_Open.Enabled = false;
+                btn_Save.Enabled = false;
+            }
+            return "";
+        }
+
+        protected void btn_Open_Click(object sender, EventArgs e)
+        {
+            //check list box selection
+            //if there is a selection
+            if (lstbx_fileSelection.SelectedIndex != -1)
+            {
+                //assign txt_SaveAs to selection
+                txtbx_SaveAs.Text = lstbx_fileSelection.SelectedItem.Text;
+
+                //read all data from file to text box
+                txtbx_editingArea.Text = File.ReadAllText(@"\MyFiles\" + lstbx_fileSelection.SelectedItem.Text);
+                //end if
+            }
+        }
+
+        protected void btn_Save_Click(object sender, EventArgs e)
+        {
+            //check txtbx isnt empty
+            if (txtbx_SaveAs.Text != "")
+            {
+                //save all txtbx data in file
+                File.WriteAllText(@"\MyFiles\" + txtbx_SaveAs.Text, txtbx_editingArea.Text);
+            }
+        }
+    }
+}
